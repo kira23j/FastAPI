08d6d6f74a68ef525d862986f9cc2a10d9ae2ba2
kira23j <kirubelmamuyesahle@gmail.com>
2024-07-10 14:13:11 -0700

    setting up the database for commercestore and do migrations

--- Django-Projects/04.drf-ecommerce/commercestore/playground/views.py
+++ Django-Projects/04.drf-ecommerce/commercestore/playground/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 # Create your views here.
 
 
 def say_hello(request):
-    return HttpResponse("hello world")
+    product=Product.objects.all()
+    return render(request,'hello.html',{'name':'kira23j'})
--- Django-Projects/04.drf-ecommerce/commercestore/store/models.py
+++ Django-Projects/04.drf-ecommerce/commercestore/store/models.py
@@ -11,8 +11,9 @@ class Collection(models.Model):
     
 class Product(models.Model):
     title=models.CharField(max_length=255)
+    slug=models.SlugField()
     description=models.TextField()
-    price=models.DecimalField(max_digits=6,decimal_places=2)
+    unit_price=models.DecimalField(max_digits=6,decimal_places=2)
     inventory=models.IntegerField()
     last_update=models.DateTimeField(auto_now=True)
     collection=models.ForeignKey(Collection,on_delete=models.PROTECT)
@@ -35,6 +36,13 @@ class Customer(models.Model):
     phone=models.CharField(max_length=255)
     birth_date=models.DateField(null=True)
     membership=models.CharField(max_length=1,choices=MEMBERSHIP_CHOICES,default=MEMBERSHIP_BRONZE)
+    
+    class Meta:
+        db_table='store_customer'
+        indexes=[
+            models.Index(fields=['last_name','first_name'])
+            ]
+
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING='P'
