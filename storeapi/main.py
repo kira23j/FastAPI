1a2261faf1764c9ccb985b86b321b356e7f9c0ac
kira23j <kirubelmamuyesahle@gmail.com>
2025-04-25 21:20:12 +0300

    implement post functionality with Pydantic models and API router

--- storeapi/main.py
+++ storeapi/main.py
@@ -1,27 +1,11 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from storeapi.routers.post import router as post_router
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to the Store API"}
 
-class UserPostIn(BaseModel):
-   body:str  
-    
-class UserPost(UserPostIn):
-    id: int
-    
-post_table = []
+app.include_router(post_router)
 
-@app.post("/", response_model=UserPost)
-async def create_post(post: UserPostIn):
-    data = post.dict()
-    last_record_id = len(post_table)
-    new_post = {**data, "id": last_record_id + 1}
-    post_table[last_record_id] = new_post
-    return new_post
-
-@app.get("/posts", response_model=list[UserPost])
-async def get_posts():
-    return list(post_table.values())
